b20a348e05b0f6c4c94afb9b0aaa20c447657858
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-07-29 18:15:43 +0300

    updated the cors

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'puma', '~> 5.0'
 gem 'rails', '~> 7.0.6'
 gem 'rubocop', '>= 1.0', '< 2.0'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
+gem 'rack-cors', require: 'rack/cors'
 
 group :development, :test do
   gem 'debug', platforms: %i[mri mingw x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greetings = Greeting.create([{ message: "Hello" }, { message: "Hola" }, { message: "Bonjour" }, { message: "Ciao" }, { message: "Konnichiwa" }, { message: "Guten Tag" }, { message: "Namaste" }, { message: "Salaam" }, { message: "Merhaba" }, { message: "Sawubona" }])
