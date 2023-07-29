96212432d17eb0d02b00e9356539e5865d04fbcc
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-07-29 16:30:31 +0300

    Sorted the linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'api/greetings/hello', to: 'api/greetings#hello'
 end
--- test/controllers/api/greetings_controller_test.rb
+++ test/controllers/api/greetings_controller_test.rb
@@ -1,8 +1,10 @@
-require "test_helper"
+require 'test_helper'
 
-class Api::GreetingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get hello" do
-    get api_greetings_hello_url
-    assert_response :success
+module Api
+  class GreetingsControllerTest < ActionDispatch::IntegrationTest
+    test 'should get hello' do
+      get api_greetings_hello_url
+      assert_response :success
+    end
   end
 end
